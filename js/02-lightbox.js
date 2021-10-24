e6be4557d7150b1c26e2e143f94560589986bbbf
Zamder13 <Zamder@ukr.net>
2021-10-24 16:43:01 +0300

    сделал 2е задание

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,11 +3,33 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);   
  
 
+const galleryContainer = document.querySelector('.gallery');
+const itemMurkup = createImageElement(galleryItems);
 
 
 
+galleryContainer.insertAdjacentHTML('beforeend', itemMurkup)
+
+
+ function createImageElement(array) {
+        return array.map(({ preview, original, description }) => {
+            return `
+    <li>
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>
+    `
+        }).join('');
+    }
 
 
 
-
-
+new SimpleLightbox('.gallery a',
+    {
+        captionsData: "alt",
+        captionPosition: 'bottom',
+        captionDelay: '250',
+        focus: false,
+        animationSpeed: '1000',
+    });
